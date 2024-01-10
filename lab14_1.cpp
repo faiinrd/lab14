d7dd84995fbf393f69870db24bcf2b5b09a429e7
faiinrd <nopradakrit@gmail.com>
2024-01-10 13:18:09 +0700

    lab14 completed

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -3,7 +3,22 @@ using namespace std;
 
 template <typename T>
 void insertionSort(T d[],int N){
-
+	int j;
+	for(int i=1; i<N; i++){
+		T key = d[i];
+		j=i-1;
+		while(j>=0 && d[j]<key){
+			d[j+1] = d[j];
+			j--;
+		}
+		d[j+1] = key;
+		cout << "Pass " << i << ":";
+		for(int i=0; i<N; i++){
+		cout << d[i] << " ";
+		}
+		cout << endl;
+	}
+	
 }
 
 int main(){
